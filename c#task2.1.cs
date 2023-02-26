e20cbd286493b8ba36f997fcb59a50c66ebc3ad6
zeyqam <zegamda@code.edu.az>
2023-02-26 21:40:13 +0400

    tasklar

---
+++ c#task2.1.cs
@@ -0,0 +1,18 @@
+﻿
+int[] arr = { 1, 8, -25, 85,- 45, 76, 48 ,0};
+int number__positive = 0;
+int number__negative = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] >= 0)
+    {
+        number__positive++;
+
+    }
+    else number__negative++;
+
+}
+
+
+Console.WriteLine("The number of negative elements:" + number__negative);
+Console.WriteLine("The number of positive elements:" + number__positive);
